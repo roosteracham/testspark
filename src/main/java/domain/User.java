f63b3b873fcfff8e261860f813c7c61bac2547e0
bkn <bbb_gbl@sf.com>
2020-03-22 15:33:20 +0800

    使用externalable 序列化、反序列化对象

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,110 @@
+package domain;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.DataOutputStream;
+import java.io.Externalizable;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+public class User implements Externalizable {
+    public static final long serializationVersionID = 1L;
+    private String name;
+    private Double salary;
+    private Integer age;
+    private String country = "ZG";
+    private String state = "HN";
+
+    public String getName() {
+        return name;
+    }
+
+    public User() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public User(String name, Double salary, Integer age) {
+        this.name = name;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(this.name.getBytes());
+        out.writeDouble(this.salary);
+        out.writeInt(this.age);
+        out.writeObject(this.country.getBytes());
+        out.writeObject(this.state.getBytes());
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        this.name = Bytes.toString((byte[]) in.readObject());
+        this.salary = in.readDouble();
+        this.age = in.readInt();
+        this.country = Bytes.toString((byte[]) in.readObject());
+        this.state = Bytes.toString((byte[]) in.readObject());
+    }
+
+    public static void main(String[] args) {
+        try {
+            String fileName = "user.txt";
+//            OutputStream outputStream = new FileOutputStream(fileName);
+//            ObjectOutputStream stream = new ObjectOutputStream(outputStream);
+//            User ab = new User("ab", 12D, 2);
+//            stream.writeObject(ab);
+//            stream.flush();;
+//            stream.close();
+
+            ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fileName));
+            User user = (User) inputStream.readObject();
+            System.out.println(JSON.toJSON(user));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
