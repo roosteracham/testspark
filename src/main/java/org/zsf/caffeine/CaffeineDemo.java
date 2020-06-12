47d7499f0c96f231131d3da0681eacf5a5450983
bkn <bbb_gbl@sf.com>
2020-06-13 00:30:51 +0800

    奇怪的过期策略

--- src/main/java/org/zsf/caffeine/CaffeineDemo.java
+++ src/main/java/org/zsf/caffeine/CaffeineDemo.java
@@ -2,31 +2,52 @@ package org.zsf.caffeine;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.Expiry;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import org.checkerframework.checker.index.qual.NonNegative;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.concurrent.TimeUnit;
 
 public class CaffeineDemo {
 
     public static void main(String[] args) throws InterruptedException {
-        Cache<String, String> cache = Caffeine.newBuilder()
+        LoadingCache<String, String> cache = Caffeine.newBuilder()
                 .initialCapacity(1)
-                .expireAfterWrite(1, TimeUnit.SECONDS)
+                .expireAfter(new Expiry<String, String>() {
+                    @Override
+                    public long expireAfterCreate(@NonNull String key, @NonNull String value, long currentTime) {
+                        return 1000 * 1000L;
+                    }
+
+                    @Override
+                    public long expireAfterUpdate(@NonNull String key, @NonNull String value, long currentTime,
+                                                  @NonNegative long currentDuration) {
+                        return Long.MAX_VALUE;
+                    }
+
+                    @Override
+                    public long expireAfterRead(@NonNull String key, @NonNull String value, long currentTime, @NonNegative long currentDuration) {
+                        return Long.MAX_VALUE;
+                    }
+                })
                 .maximumSize(1)
                 .removalListener((k, v, cause) -> {
-                    System.out.println(k + ": " + v + " removed due to " + cause.name());
+                    System.out.println(k + ": " + v + ", removed due to " + cause.name());
                 })
                 .softValues()
                 .build(String::toUpperCase);
 
         cache.put("a", "asd");
 
-        System.out.println(cache.getIfPresent("b"));
+        System.out.println(cache.get("b"));
         System.out.println(cache.get("b", e -> e));
-        System.out.println(cache.getIfPresent("b"));
-        System.out.println(cache.getIfPresent("a"));
+        System.out.println(cache.get("b"));
+        System.out.println(cache.get("a"));
 
         TimeUnit.SECONDS.sleep(2);
-        System.out.println(cache.getIfPresent("a"));
-        System.out.println(cache.getIfPresent("c"));
+        System.out.println(cache.get("a"));
+        System.out.println(cache.get("b"));
+        System.out.println(cache.get("c"));
     }
 }
